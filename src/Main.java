3c41e9ae8979078e8d9fa70e6a1b0ee4dbed8a3c
shivamanesh <shivamanesh93@gmail.com>
2022-04-20 21:58:56 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rating rating = new Rating();
+        Map<Integer, Integer> movieRating = new HashMap<>();
+        rating.addRating(3);
+       rating.addRating(3);
+       rating.addRating(4);
+        System.out.println("this is the whole amount: " + rating.getAmountRatings());
+        System.out.println("This is the whole amount of specific stars: " + rating.getAmountRatings(3));
+        System.out.println("This is the average of ratings: " + rating.getAverageRating());
+    }
+}
---
+++ src/Rating.java
@@ -0,0 +1,49 @@
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Rating {
+    //key = rating (which is between 0 and 5), value = the num of people who are rating
+    Map<Integer, Integer> ratings = new HashMap<>();
+
+    public void addRating(int stars){
+        if(stars >= 0 && stars <= 5){
+            if( ratings.containsKey(stars)){
+                ratings.put(stars, ratings.get(stars) + 1);
+            }
+            else{
+                ratings.put(stars, 1);
+            }
+        }
+        else{
+            System.out.println("not in range!");
+        }
+    }
+
+    public int getAmountRatings(){
+        int sum = 0;
+        for (int rating: ratings.keySet()) {
+            sum += ratings.get(rating) * rating;
+        }
+        return sum;
+    }
+
+    public int getAmountRatings(int stars){
+        int sumOfSpecificRating = 0;
+        sumOfSpecificRating = stars * ratings.get(stars);
+        return sumOfSpecificRating;
+    }
+
+    public float getAverageRating(){
+        int numberOfRatings = 0;
+        float averageOfRatings;
+        for (int value: ratings.values()) {
+            numberOfRatings += value;
+        }
+        averageOfRatings = (float)getAmountRatings() / numberOfRatings;
+        return averageOfRatings;
+    }
+
+}
